eeb1058dbf1ea74a92b747957327634b85d4b0d1
ArkadiuszKolodynski <ArkadiuszKolodynski@users.noreply.github.com>
2024-06-28 14:56:49 +0200

    feat: add blog card component & styles

---
+++ src/components/blog-card/BlogCard.tsx
@@ -0,0 +1,40 @@
+import articleImage from "../../assets/images/illustration-article.svg";
+import avatarImage from "../../assets/images/image-avatar.webp";
+import classNames from "./BlogCard.module.scss";
+
+function BlogCard() {
+  return (
+    <div className={`${classNames.blogCard}`}>
+      <img
+        src={articleImage}
+        alt="article"
+        className={classNames.articleImage}
+      />
+      <div className={classNames.content}>
+        <div>
+          <span className={classNames.learningBadge}>Learning</span>
+        </div>
+        <div className={classNames.published}>Published 21 Dec 2023</div>
+        <h1 className={classNames.heading}>
+          <a href="#" className={classNames.title}>
+            HTML & CSS foundations
+          </a>
+        </h1>
+        <p className={classNames.description}>
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+      </div>
+      <div className={classNames.author}>
+        <img
+          src={avatarImage}
+          alt="avatar"
+          className={classNames.avatarImage}
+        />
+        <div className={classNames.authorName}>Greg Hooper</div>
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
